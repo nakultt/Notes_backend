d58cec8fa4c7c938bf28d45b104cd1544de2c22b
nakultt <nakulnuked@gmail.com>
2025-09-05 18:28:24 +0530

    feat: include email in login response

--- auth.ts
+++ auth.ts
@@ -44,7 +44,7 @@ router.post("/login" , async (req, res) => {
 
         const token = jwt.sign({userId: user._id, email: user.email}, process.env.JWT_SECRET!)
 
-        res.status(200).json({message: "Login successful", token})
+        res.status(200).json({message: "Login successful", token, email})
     } catch (err) {
         res.status(500).json({message: "Server error", err})
     }
